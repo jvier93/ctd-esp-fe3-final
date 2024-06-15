07deaf54e60a3b4b33a1eb6bacbcaeb0411c1cfa
Pablo <pablo-krai@hotmail.com>
2024-06-15 16:28:34 -0300

    Agregada rutas y persistencia de navbar y footer con react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Detail from "./Routes/Detail";
+import Favs from "./Routes/Favs";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+
+      {
+        path: "/contact",
+        element: <Contact></Contact>,
+      },
+      {
+        path: "/detail/:id",
+        element: <Detail></Detail>,
+      },
+      {
+        path: "/favs",
+        element: <Favs></Favs>,
+      },
+    ],
+  },
+]);
+
 root.render(
   <React.StrictMode>
-      <App/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-
